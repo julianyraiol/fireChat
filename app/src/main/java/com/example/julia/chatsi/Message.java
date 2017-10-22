ab22a027282771008a59a9f1992ed6103dd6e731
Juliany Raiol <julianyraiol@gmail.com>
2017-10-22 17:13:05 -0400

    feat: Login com Firebase

---
+++ app/src/main/java/com/example/julia/chatsi/Message.java
@@ -0,0 +1,46 @@
+package com.example.julia.chatsi;
+
+/**
+ * Created by julia on 12/10/2017.
+ */
+
+public class Message {
+
+    private String text;
+    private String name;
+    private String photoUrl;
+
+    public Message(){
+
+    }
+
+    public Message(String text, String name, String photoUrl){
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
---
+++ app/src/main/java/com/example/julia/chatsi/MessageAdapter.java
@@ -0,0 +1,59 @@
+package com.example.julia.chatsi;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by julia on 12/10/2017.
+ */
+
+public class MessageAdapter extends ArrayAdapter<Message>{
+
+    public MessageAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Message> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        if(convertView == null){
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+        }
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+
+        Message message = getItem(position);
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+
+        if (isPhoto){
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(photoImageView);
+        }
+        else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        }
+        authorTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
